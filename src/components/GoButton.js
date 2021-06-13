547d3dacb4e49350cc83bc73ac5b54cece9e778e
Your Name <timon_a@hotmail.com>
2021-06-13 09:46:20 +0200

    added return to function

--- src/components/GoButton.js
+++ src/components/GoButton.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
 
 export const GoButton = ({name, street, postal, lon, lat, cN}) => {
-    const prepareElements = (e) => {
-        e.trim().split(' ').join('+')
+
+    const prepareElements = (s) => {
+        return s.trim().split(' ').join('+')
     }
     const url = `https://www.google.nl/maps/dir/${lat},${lon}/${prepareElements(name)},${prepareElements(street)},${prepareElements(postal)}/data=!4m2!4m1!3e2`
     return (
-        <Link to={{pathname: url}} target="_blank">
-            <button className={cN} type='button'>Go!</button>
-        </Link>
+        <button
+            className={cN}
+            type='button'
+            onClick={() => window.open(url, '_blank')}
+        >Go!
+        </button>
     )
 }
